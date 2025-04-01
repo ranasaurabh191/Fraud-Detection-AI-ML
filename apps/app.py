5bcb747d3d3306275d19d9c1f0cc8914a8c7247a
Saurabh Rana <ranasaurabh191@gmail.com>
2025-04-01 23:18:53 +0530

    Update app.py

--- apps/app.py
+++ apps/app.py
@@ -93,6 +93,7 @@ def predict():
         import traceback
         print("🔥 Error:", traceback.format_exc())  # Print detailed traceback for debugging
         return jsonify({"error": str(e)}), 500
-
+import os
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get("PORT", 10000))  # Use Render's assigned port
+    app.run(host='0.0.0.0', port=port)
